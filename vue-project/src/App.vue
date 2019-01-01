a135b980bc2d32dce5a168fd93443bc4412bf9c1
srchach <gsikmo@gmail.com>
2019-01-01 00:17:18 -0600

    vue-project: added validations to props

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,28 +1,31 @@
 <template>
 	<div id="app">
-		<List :titulo="otro_titulo"></List>
+		<!-- Hace un bind con la propiedad titulo y se la pasa al hijo -->
+		<app-list class="List" :titulo="otro_titulo"></app-list>
 	</div>
 </template>
 
 <script>
-import List from './ListComponent.vue'
+//	Como tiene el export default, le damos el nombre que queramos a lo que importamos
+import appList from './ListComponent.vue'
+
 export default {
+	/*	Aunque importemos, se carga el componente solo cuando lo especificamos en 'components'
+	 *	Vue convierte cualquier componente en camel case, dentro del template, a separado por guiones
+	 */
 	components: {
-		List
+		appList
 	},
 	data(){
 		return { otro_titulo: 'I\'m a Title c:' }
 	}
-	/*	Es lo mismo que lo de arriba
-		data: function(){
-			return {
-				otro_titulo: 'Title c:'
-			}
-		}
-	*/
+	/*	data: function(){} 
+	 *		es lo mismo que 
+	 *	data(){}
+	 */
 }
 </script>
 
 <style lang="scss">
-
+	.List { border: 1px solid red; }
 </style>
--- vue-project/src/ListComponent.vue
+++ vue-project/src/ListComponent.vue
@@ -1,11 +1,70 @@
 <template>
-	<h2>{{ titulo }}</h2>
+	<div>
+		<h2>{{ tituloMayusculas() }}</h2>
+		<input type="text" v-model="nueva_tarea" @keyup.enter="anadir()"> 
+		<button @click='anadir()'>Agregar</button>
+		<ul>
+			<li v-for="(tarea, indice) in tareas">
+				<span :class="{ terminada : !tarea.isActive}">{{ tarea.titulo }}</span> 
+				<button @click="tarea.isActive = !tarea.isActive">
+					<span v-if="tarea.isActive">check</span>
+					<span v-else>uncheck</span>
+				</button>
+				<button @click="eliminar(indice)">x</button>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 	export default {
-		props: [
-			"titulo"
-		]
+		/*	props: [ 'service_name' ] es sin validaciones
+		 *	props: { 'something' } es para validaciones
+		 */
+		props: {
+			titulo: {
+				//	required: true,
+				type: String,
+				default: "Lista de Tareas"
+			}
+		},
+		data(){
+			return { 
+				tareas: [
+					{
+						titulo: "Mejorar mi apariencia",
+						isActive: true
+					},
+					{
+						titulo: "Mejorar mi comunicación no verbal",
+						isActive: true
+					}
+				],
+				nueva_tarea: ""
+			}
+		},
+		methods: {
+			anadir: function(){
+				if(this.nueva_tarea.trim() != '')
+					this.tareas.push({
+						titulo: this.nueva_tarea,
+						isActive: true
+					})
+				this.nueva_tarea = ''
+			},
+			eliminar: function(indice){
+				this.tareas.splice(indice, 1)
+			},
+			tituloMayusculas(){
+				return this.titulo.toUpperCase()
+			}
+		}
 	}
 </script>
+
+<!-- 
+	Con 'scoped' forzamos que el estilo solo pueda ser usado en este componente
+	Se pueden tener en el mismo componente ambos 'style', scoped y normal -->
+<style>
+	.terminada { text-decoration: line-through; }
+</style>
