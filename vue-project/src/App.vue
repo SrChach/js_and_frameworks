d5fbfa75b6e6ed79e0146a8e3b51bedc65591d2c
srchach <gsikmo@gmail.com>
2018-12-31 23:13:32 -0600

    Vue project, communication father->son

---
+++ vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="app">
+		<List :titulo="otro_titulo"></List>
+	</div>
+</template>
+
+<script>
+import List from './ListComponent.vue'
+export default {
+	components: {
+		List
+	},
+	data(){
+		return { otro_titulo: 'I\'m a Title c:' }
+	}
+	/*	Es lo mismo que lo de arriba
+		data: function(){
+			return {
+				otro_titulo: 'Title c:'
+			}
+		}
+	*/
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ vue-project/src/ListComponent.vue
@@ -0,0 +1,11 @@
+<template>
+	<h2>{{ titulo }}</h2>
+</template>
+
+<script>
+	export default {
+		props: [
+			"titulo"
+		]
+	}
+</script>
